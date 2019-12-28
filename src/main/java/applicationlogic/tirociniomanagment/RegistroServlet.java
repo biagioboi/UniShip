9be870ab771e200c5210344a177c3161bc867387
Andrea Mennillo <handymenny@outlook.com>
2019-12-28 10:30:00 +0100

    applicationlogin: Other checkstyle fixes

--- src/main/java/applicationlogic/tirociniomanagment/RegistroServlet.java
+++ src/main/java/applicationlogic/tirociniomanagment/RegistroServlet.java
@@ -1,12 +1,11 @@
 package applicationlogic.tirociniomanagment;
 
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet("/RegistroServlet")
 public class RegistroServlet extends HttpServlet {
