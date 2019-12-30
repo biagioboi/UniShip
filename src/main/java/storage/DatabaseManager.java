93426d129fa714c49088be1dff70bfbea38ecf86
Andrea Mennillo <handymenny@outlook.com>
2019-12-30 13:11:55 +0100

    DatabaseManager: Set driverclassname

--- src/main/java/storage/DatabaseManager.java
+++ src/main/java/storage/DatabaseManager.java
@@ -21,6 +21,8 @@ public class DatabaseManager {
     config.addDataSourceProperty("serverTimezone", "UTC");
     config.addDataSourceProperty("allowPublicKeyRetrieval", "true");
     config.addDataSourceProperty("leakDetectionThreshold", "6000");
+    config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
     ds = new HikariDataSource(config);
   }
 
