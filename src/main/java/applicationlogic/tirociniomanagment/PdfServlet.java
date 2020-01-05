07c72f891770961bfda31893f25b9f626ee712ad
= <gerardo.gullo@gmail.com>
2020-01-05 14:06:17 +0100

    some changes

--- src/main/java/applicationlogic/tirociniomanagment/PdfServlet.java
+++ src/main/java/applicationlogic/tirociniomanagment/PdfServlet.java
@@ -23,10 +23,16 @@ import storage.beans.Azienda;
 import storage.beans.Studente;
 import storage.beans.Tirocinio;
 import storage.beans.Utente;
+import storage.dao.AziendaDao;
 import storage.dao.RichiestaDisponibilitaDao;
+import storage.dao.StudenteDao;
 import storage.dao.TirocinioDao;
+import storage.dao.UtenteDao;
+import storage.interfaces.AziendaInterface;
 import storage.interfaces.RichiestaDisponibilitaInterface;
+import storage.interfaces.StudenteInterface;
 import storage.interfaces.TirocinioInterface;
+import storage.interfaces.UtenteInterface;
 
 //todo : da cambiare il nome sulla documetazione per createPDF in createPdf
 @WebServlet("/PDFServlet")
@@ -58,7 +64,7 @@ public class PdfServlet extends HttpServlet {
       throws ServletException, IOException {
 
     String numeroCfu = request.getParameter("cfu");
-    if (numeroCfu == null) {
+    if (numeroCfu == null || numeroCfu.length() == 0) {
       throw new IllegalArgumentException("Inserire i cfu.");
     }
     if (!numeroCfu.matches("[0-9]{1,2}")) {
@@ -66,70 +72,70 @@ public class PdfServlet extends HttpServlet {
     }
 
     String sedeSvolgimento = request.getParameter("sede");
-    if (sedeSvolgimento == null) {
+    if (sedeSvolgimento == null || sedeSvolgimento.length() == 0) {
       throw new IllegalArgumentException("Inserire una sede di svolgimento.");
     } else if (!sedeSvolgimento.matches("[A-z0-9 ,]")) {
       throw new IllegalArgumentException("Sede svolgimento non valida.");
     }
 
     String obiettivi = request.getParameter("obiettivi");
-    if (obiettivi == null) {
+    if (obiettivi == null || obiettivi.length() == 0) {
       throw new IllegalArgumentException("Inserire gli obiettivi.");
     } else if (!obiettivi.matches("[A-z0-9 ,.;]")) {
       throw new IllegalArgumentException("Formato degli obiettivi non valido.");
     }
 
     String competenze = request.getParameter("competenze");
-    if (competenze == null) {
+    if (competenze == null || competenze.length() == 0) {
       throw new IllegalArgumentException("Inserire le competenze.");
     } else if (!competenze.matches("[A-z0-9 ,.;]")) {
       throw new IllegalArgumentException("Formato delle competenze non valido.");
     }
 
     String attivita = request.getParameter("attivita");
-    if (attivita == null) {
+    if (attivita == null || attivita.length() == 0) {
       throw new IllegalArgumentException("Inserire le attivit&agrave;.");
     } else if (!attivita.matches("[A-z0-9 ,.;]")) {
       throw new IllegalArgumentException("Formato delle attivit&agrave; non valido.");
     }
 
     String modalita = request.getParameter("modalita");
-    if (modalita == null) {
+    if (modalita == null || modalita.length() == 0) {
       throw new IllegalArgumentException("Inserire le modalit&agrave; .");
     } else if (!modalita.matches("[A-z0-9 ,.;]")) {
       throw new IllegalArgumentException("Formato delle modalit&agrave; non valido.");
     }
 
     String dataInizio = request.getParameter("dataInizio");
-    if (dataInizio == null) {
+    if (dataInizio == null || dataInizio.length() == 0) {
       throw new IllegalArgumentException("Inserire la data di inizio.");
     } else if (!dataInizio.matches("([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))")) {
       throw new IllegalArgumentException("Formato delle data di inizio non valida.");
     }
 
     String dataFine = request.getParameter("dataFine");
-    if (dataFine == null) {
+    if (dataFine == null || dataFine.length() == 0) {
       throw new IllegalArgumentException("Inserire la data di fine.");
     } else if (!dataFine.matches("([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))")) {
       throw new IllegalArgumentException("Formato delle data di fine non valida.");
     }
 
     String orario = request.getParameter("orario");
-    if (orario == null) {
+    if (orario == null || orario.length() == 0) {
       throw new IllegalArgumentException("Inserire l'orario di lavoro.");
     } else if (!orario.matches("[A-z0-9 ,.;]")) {
       throw new IllegalArgumentException("Formato dell'orario non valido.");
     }
 
     String numeroRc = request.getParameter("numeroRc");
-    if (numeroRc == null) {
+    if (numeroRc == null || numeroRc.length() == 0) {
       throw new IllegalArgumentException("Inserire il numero RC.");
     } else if (!numeroRc.matches("[0-9]")) {
       throw new IllegalArgumentException("Formato del numero RC non valido.");
     }
 
     String polizza = request.getParameter("polizza");
-    if (polizza == null) {
+    if (polizza == null || polizza.length() == 0) {
       throw new IllegalArgumentException("Inserire la polizza assicurativa infortuni.");
     } else if (!obiettivi.matches("[A-z0-9 ,.;]")) {
       throw new IllegalArgumentException(
@@ -143,19 +149,23 @@ public class PdfServlet extends HttpServlet {
     TemplateEngine templateEngine = new TemplateEngine();
     templateEngine.setTemplateResolver(templateResolver);
 
-    String id = request.getParameter("tirocinio");
-    if (id == null) {
-      throw new IllegalArgumentException("Inserire un tirocinio.");
-    }
-    if (id.length() < 1) {
-      throw new IllegalArgumentException("id non valido.");
+    String emailStudente = request.getParameter("studente");
+    if (emailStudente == null || emailStudente.length() == 0) {
+      throw new IllegalArgumentException("Email dello studente non puo' essere vuota");
     }
 
     try {
-      TirocinioInterface tirocinioDao = new TirocinioDao();
-      Tirocinio tirocinio = tirocinioDao.doRetrieveByKey(Integer.parseInt(id));
-      Azienda azienda = tirocinio.getAzienda();
-      Studente studente = tirocinio.getStudente();
+      StudenteInterface studenteDao = new StudenteDao();
+      AziendaInterface aziendaDao = new AziendaDao();
+      UtenteInterface utenteDao = new UtenteDao();
+
+      if (!utenteDao.doCheckRegister(emailStudente)) {
+        throw new IllegalArgumentException("Email studente errata");
+      }
+
+      Utente user = (Utente) request.getSession().getAttribute("utente");
+      Studente studente = studenteDao.doRetrieveByKey(emailStudente);
+      Azienda azienda = aziendaDao.doRetrieveByKey(user.getEmail());
 
       Context context = new Context();
       context.setVariable("nomeAzienda", azienda.getNome());
