c824628c1754434a72a2e17b73af9d47dfe0cd8c
= <gerardo.gullo@gmail.com>
2020-01-07 13:20:11 +0100

    some fix

--- src/main/webapp/js/azienda.js
+++ src/main/webapp/js/azienda.js
@@ -45,7 +45,7 @@ $(() => {
     let rhours = Math.floor(hours);
     let minutes = (hours - rhours) * 60;
     let rminutes = Math.round(minutes);
-    return rhours + rminutes;
+    return rhours +":"+ rminutes;
   }
 
   $("#btnInviaProgettoF").click(() => {
