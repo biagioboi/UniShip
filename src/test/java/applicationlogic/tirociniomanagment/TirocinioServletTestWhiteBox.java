4ed1d4e5dc4b986cc7bee4eb675e7c39dd1a0525
= <gerardo.gullo@gmail.com>
2020-01-14 14:42:39 +0100

    TirocinioServletTestWhiteBox started

---
+++ src/test/java/applicationlogic/tirociniomanagment/TirocinioServletTestWhiteBox.java
@@ -0,0 +1,9 @@
+package applicationlogic.tirociniomanagment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.mockito.Mockito;
+
+public class TirocinioServletTestWhiteBox extends Mockito {
+
+}
