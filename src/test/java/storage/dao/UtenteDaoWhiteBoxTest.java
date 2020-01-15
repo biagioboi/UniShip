e84f9c0d191b21964988751c988fe712b559f121
= <gerardo.gullo@gmail.com>
2020-01-15 11:33:45 +0100

    added UtenteDaoWhiteBoxTest

---
+++ src/test/java/storage/dao/UtenteDaoWhiteBoxTest.java
@@ -0,0 +1,53 @@
+package storage.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+
+class UtenteDaoWhiteBoxTest {
+
+  private static UtenteDao dao = new UtenteDao();
+
+  @Test
+  void doRetrieveAll() throws SQLException {
+    assertEquals(5,dao.doRetrieveAll().size());
+  }
+
+  @Test
+  void doRetrieveByKeyNull() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      dao.doRetrieveByKey(null);
+    });
+  }
+
+  @Test
+  void doSaveNull() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      dao.doSave(null);
+    });
+  }
+
+  @Test
+  void doCheckRegisterNull() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      dao.doCheckRegister(null);
+    });
+  }
+
+  @Test
+  void doCheckLogin() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      dao.doCheckLogin(null,null);
+    });
+  }
+
+  @Test
+  void doCheckLogin1() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      dao.doCheckLogin("email",null);
+    });
+  }
+
+
+}
